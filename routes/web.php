d469ecdb527ab07d305ade633f320684629e8f04
brunobwn <brunoberwian@gmail.com>
2023-02-13 12:43:50 -0300

    add orders routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
 Route::resource('products', 'ProductController')->middleware('auth');
 Route::resource('categories', 'CategoryController')->middleware('auth');
+Route::resource('orders', 'OrderController')->middleware('auth');
