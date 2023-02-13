2f8007e8a022773088ef271bafe6e1f6ccfa9d3c
brunobwn <brunoberwian@gmail.com>
2023-02-13 11:08:30 -0300

    create pivot ordes_products table

---
+++ database/migrations/2023_02_13_134809_create_orders_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->integer('product_id')->unsigned()->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('SET NULL');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('quantity');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_products');
+    }
+}
