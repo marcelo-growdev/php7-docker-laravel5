d457197bfef85531f034da05d9a23eb9474af327
brunobwn <brunoberwian@gmail.com>
2023-02-14 18:03:19 -0300

    add event log on new order

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\NewOrderEvent;
+use App\Listeners\CountOrder;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -18,6 +20,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        NewOrderEvent::class => [CountOrder::class]
     ];
 
     /**
